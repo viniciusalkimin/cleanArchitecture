802ff10266bdc0c6f7e97ebd4f4533f17d55c515
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-07-04 18:40:32 -0300

    Refactor AlunoTest

--- src/test/java/com/escola/dominio/aluno/AlunoTest.java
+++ src/test/java/com/escola/dominio/aluno/AlunoTest.java
@@ -2,6 +2,7 @@ package com.escola.dominio.aluno;
 
 import com.escola.dominio.exception.ViolacaoDeDominioException;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class AlunoTest {
 
     public AlunoFactory alunoFactory = new AlunoFactory();
+
+    public Aluno aluno;
+
+    @BeforeEach
+    void inicializar() {
+        this.aluno = alunoFactory.comNomeCPFEmail("Vinicius", "123.456.789-00", "v@email.com").criar();
+    }
     @Test
     void deveriaCadastrarTelefoneComSucesso() {
-        Aluno aluno = alunoFactory.comNomeCPFEmail("Vinicius", "123.456.789-00", "v@email.com").criar();
         aluno.adicionarTelefone("11", "91111-1111");
         aluno.adicionarTelefone("11", "2222-2222");
         assertEquals(2, aluno.getTelefones().size());
@@ -19,7 +26,6 @@ class AlunoTest {
 
     @Test
     void deveriaLancarViolacaoDeDominioExceptionQuandoAddTresTelefones() {
-        Aluno aluno = alunoFactory.comNomeCPFEmail("Vinicius", "123.456.789-00", "v@email.com").criar();
         aluno.adicionarTelefone("11", "91111-1111");
         aluno.adicionarTelefone("11", "2222-2222");
         assertThrows(ViolacaoDeDominioException.class, () -> aluno.adicionarTelefone("11", "3333-3333"));
